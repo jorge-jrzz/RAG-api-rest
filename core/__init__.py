cbfeb5d1609a2f8ff73298a481ad1a166fd2f983
Jorge Ángel Juárez Vázquez <jorgeang33@gmail.com>
2024-10-06 18:31:55 -0600

    :coffin: remove unnecesary module import

--- core/__init__.py
+++ core/__init__.py
@@ -1,4 +1,4 @@
-from .files_strategy import FileManager, Strategy, AcceptedFiles, Another
+from .files_strategy import FileManager, AcceptedFiles, Another
 from .OCR import OCR
 from .TextChunk import TextChunk
 from .utils import get_logger
