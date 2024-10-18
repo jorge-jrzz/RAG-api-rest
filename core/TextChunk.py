9df2ae62edb9e0956754a891e1ba204e116c289e
Jorge Ángel Juárez Vázquez <jorgeang33@gmail.com>
2024-10-17 21:34:07 -0600

    ♻️ Refactor type methods and add log messages
    💡 refactor docstrings

--- core/TextChunk.py
+++ core/TextChunk.py
@@ -1,23 +1,30 @@
+"""
+This module contains the TextChunk class, which is used to handle text chunks and add them to a Polars DataFrame
+"""
+
 import json
 import copy
 import sqlite3
 from typing import List, Dict, Optional, Union
-from sqlalchemy import create_engine
 import polars as pl
+from sqlalchemy import create_engine
+from .utils import get_logger
 
+
+logger = get_logger(__name__)
 
 class TextChunk():
     """
     Class to handle text chunks and add them to a Polars DataFrame
     """
-    def __init__(self, current_df: Optional[pl.DataFrame] = pl.DataFrame()):
-        self.current_df = current_df
     
-    def _pdf_chunk(self, json_data: List[Dict]) -> pl.DataFrame:
+    @classmethod
+    def _pdf_chunk(cls, data_df: pl.DataFrame, json_data: List[Dict]) -> pl.DataFrame:
         """
         Create a DataFrame from a list of dictionaries with text chunks from a PDF file
 
         Args:
+            data_df (pl.DataFrame): DataFrame that will be updated with the new data (Current DataFrame)
             json_data (List[Dict]): List of dictionaries with text chunks from a PDF file
         
         Returns:
@@ -25,37 +32,52 @@ class TextChunk():
         """
         for item in json_data:
             item['metadata'] = json.dumps(item['metadata'])
+            # Decodificar el texto a UTF-8
             item['text'] = item['text'].decode('utf-8')
         # Crea el DataFrame de Polars con los datos y un índice que empieza en el último índice del DataFrame actual
-        return pl.DataFrame(json_data).with_row_index('id', offset=len(self.current_df)+1)
+        new_df = pl.DataFrame(json_data).with_row_index('id', offset=len(data_df)+1)
+        logger.info("DataFrame created from PDF chunks")
+        return new_df
 
-    def _add_if_not_exists(self, new_data: Union[pl.DataFrame, Dict], key_columns: Optional[List] = ['metadata', 'text']) -> pl.DataFrame:
+    @classmethod
+    def _add_if_not_exists(cls, data_df: pl.DataFrame, 
+                           new_data: Union[pl.DataFrame, Dict], 
+                           key_columns: Optional[List] = None) -> pl.DataFrame:
         """
-        Add new data to the current DataFrame if it does not exist
+        Add new data to the current DataFrame if it does not exist. 
 
         Args:
+            data_df (pl.DataFrame): DataFrame that will be updated with the new data (Current DataFrame)
             new_data (Union[pl.DataFrame, Dict]): New data to add to the DataFrame
-            key_columns (Optional[List]): Columns to use as keys to identify if the data already exists, by default ['metadata', 'text']
-        
+            key_columns (Optional[List]): List of columns to use as keys to identify existing data, by default None -> ['metadata', 'text']
+
         Returns:
-            pl.DataFrame: Currently updated polars DataFrame
+            pl.DataFrame: Updated DataFrame with the new data added or the same DataFrame if no new data is found
+
+        Raises:
+            TypeError: If new_data is not a DataFrame or a dictionary
         """
+        # Inicializar la lista de columnas clave si no se proporciona
+        if key_columns is None:
+            key_columns = ['metadata', 'text']
         # Si nuevos_datos es un diccionario, convertirlo a DataFrame, si no, verificar que sea un DataFrame
         if isinstance(new_data, dict):
             new_data = pl.DataFrame([new_data])
         elif not isinstance(new_data, pl.DataFrame):
-            raise TypeError("nuevos_datos debe ser un DataFrame de Polars o un diccionario")
+            logger.error("new_data must be a Polars DataFrame or a dictionary")
+            raise TypeError("new_data must be a Polars DataFrame or a dictionary")
         # Si el DataFrame actual está vacío, asignarle los nuevos datos y retornarlo
-        if self.current_df.is_empty():
-            self.current_df = self.current_df.vstack(new_data)
-            return self.current_df
+        if data_df.is_empty():
+            data_df = data_df.vstack(new_data)
+            logger.info("New data added to the DataFrame")
+            return data_df
         # Crear una expresión para verificar si los datos ya existen
         condition = pl.all_horizontal([
             pl.col(col).is_in(new_data[col])
             for col in key_columns
         ])
         # Filtrar los datos existentes
-        existing_data = self.current_df.filter(condition)
+        existing_data = data_df.filter(condition)
         # Identificar los datos nuevos
         new = new_data.join(
             existing_data.select(key_columns),
@@ -64,50 +86,66 @@ class TextChunk():
         )
         # Si hay datos nuevos, agregarlos al DataFrame original
         if not new.is_empty():
-            print("Se han encontrado datos nuevos para agregar")
-            self.current_df = pl.concat([self.current_df, new], how="vertical")
+            logger.info("New data found to add")
+            data_df = pl.concat([data_df, new], how="vertical")
         else:
-            print("No hay datos nuevos para agregar")
-        return self.current_df
+            logger.warning("No new data to add")
+        return data_df
     
-    def add_chunks_to_dataframe(self, json_data: List[Dict]) -> pl.DataFrame:
+    @classmethod
+    def add_chunks_to_dataframe(cls, data_df: pl.DataFrame, json_data: List[Dict]) -> pl.DataFrame:
         """
         Add text chunks to the current DataFrame. The method will identify the type of file and call the corresponding method to process the data
 
         Args:
+            data_df (pl.DataFrame): DataFrame that will be updated with the new data (Current DataFrame)
             json_data (List[Dict]): List of dictionaries with text chunks
         
         Returns:
-            pl.DataFrame: Currently updated polars DataFrame
+            pl.DataFrame: Currently updated polars DataFrame with the new data added or the same DataFrame if the filetype is not supported
         """
+
         filetype = json_data[0]['metadata']['filetype']
+        df = None
         if filetype == "application/pdf":
-            df = self._pdf_chunk(json_data)
+            df = cls._pdf_chunk(data_df, json_data)
+            logger.info("PDF chunks added to the DataFrame")
         elif filetype.startswith('text'):
+            # Crear una copia profunda de los datos para evitar modificar el original
             data = copy.deepcopy(json_data)
             data[0]['metadata'] = json.dumps(data[0]['metadata'])
-            df = pl.DataFrame(data).with_row_index('id', offset=len(self.current_df)+1)
-        self._add_if_not_exists(new_data=df)
-        return self.current_df
+            df = pl.DataFrame(data).with_row_index('id', offset=len(data_df)+1)
+            logger.info("Text chunks added to the DataFrame")
+        if df is not None:
+            updated_df = cls._add_if_not_exists(data_df, new_data=df)
+            logger.info("Data added to the DataFrame")
+            return updated_df
+        logger.error("Filetype not supported")
+        return data_df
 
-    def save_checkpoint(self, checkpoint_path: str, table_name: Optional[str] = 'ocr_data') -> None:
+    @classmethod
+    def save_checkpoint(cls, data_df: pl.DataFrame, checkpoint_path: str, table_name: Optional[str] = 'ocr_data') -> None:
         """
         Save the current DataFrame to a SQLite database
 
         Args:
+            data_df (pl.DataFrame): DataFrame to save to the SQLite database (current DataFrame)
             checkpoint_path (str): Path to the SQLite database
             table_name (Optional[str]): Name of the table to store the data, by default 'ocr_data'
 
         Returns:
             None
         """
+
         conn = sqlite3.connect(checkpoint_path)
-        temp_df = self.current_df.clone()
+        temp_df = data_df.clone()
         temp_df.drop_in_place('id')
         temp_df.write_database(table_name=table_name, connection=f"sqlite:///{checkpoint_path}", if_table_exists="replace")
         conn.close()
+        logger.info("DataFrame saved to SQLite database")
     
-    def load_checkpoint(self, checkpoint_path: str, table_name: Optional[str] = 'ocr_data') -> pl.DataFrame:
+    @classmethod
+    def load_checkpoint(cls, checkpoint_path: str, table_name: Optional[str] = 'ocr_data') -> pl.DataFrame:
         """
         Load a DataFrame from a SQLite database
 
@@ -118,7 +156,8 @@ class TextChunk():
         Returns:
             pl.DataFrame: DataFrame loaded from the SQLite database (current DataFrame)
         """
+
         conn = create_engine(f"sqlite:///{checkpoint_path}")
         query = f"SELECT * FROM {table_name}"
-        self.current_df = pl.read_database(query=query, connection=conn.connect()).with_row_index('id')
-        return self.current_df
+        data_df = pl.read_database(query=query, connection=conn.connect()).with_row_index('id')
+        return data_df
