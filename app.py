a7011911baedc6737227b613f144e534de77ae59
Jorge Ángel Juárez Vázquez <jorgeang33@gmail.com>
2024-10-17 22:37:46 -0600

    ♻️ Refactor core classes use un RAG API
     - 💡 Add docstrings to functions

--- app.py
+++ app.py
@@ -1,27 +1,57 @@
+"""
+This module contains the main code for the RAG API
+"""
+
 import os
 from typing import Optional, List, Dict
 import json
 import polars as pl
 from werkzeug.utils import secure_filename
 from flask import Flask, request, jsonify
-from core import FileManager, AcceptedFiles, Another, OCR, TextChunk, MilvusManager
+from core import FileManager, AcceptedFiles, Another, OCR, TextChunk, MilvusManager, get_logger, delete_files_directory
 
 
 UPLOAD_FOLDER = './uploads'
-ALLOWED_EXTENSIONS = {'txt', 'html', 'md', 'java', 'py', 'c', 'cpp', 'js', 'pdf', 'png', 'jpg', 'jpeg', 'ppt', 'pptx', 'doc', 'docx'}
+ALLOWED_EXTENSIONS = ['txt', 'html', 'md', 'java', 'py', 'c', 'cpp', 'js', 'pdf', 
+                      'png', 'jpg', 'jpeg', 'ppt', 'pptx', 'doc', 'docx']
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+milvus_manager = MilvusManager()
+logger = get_logger(__name__)
 
 
-def allowed_file(filename):
+def allowed_file(filename: str, extensions: List[str]) -> bool:
+    """
+    Function to check if the file format is allowed.
+
+    Args:
+        extensions (List[str]): The list of allowed extensions.
+        filename (str): The name of the file.
+
+    Returns:
+        bool: True if the file format is allowed, False otherwise.
+    """
+
+    # Primero crea una lista con el nombre del archivo y la extensión
+    # Selecciona el segundo elemento de la lista, que es la extensión y la convierte a minúsculas
+    # Comprueba si la extensión está en la lista de extensiones permitidas
     return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+           filename.rsplit('.', 1)[1].lower() in extensions
 
 def ensure_file_format(file: str) -> Optional[str]:
+    """
+    Function to aplly the strategy pattern to move the file to the destination directory.
+
+    Args:
+        file (str): The path of the file.
+
+    Returns:
+        Optional[str]: The path of the file moved to the destination directory.
+    """
+
     file_manager = FileManager()
-    if '.' in file and file.rsplit('.', 1)[1].lower() \
-            in ['pdf', 'txt', 'html', 'md', 'java', 'py', 'c', 'cpp', 'js']:
+    if allowed_file(file, ALLOWED_EXTENSIONS[:9]):
         file_manager.set_strategy(AcceptedFiles())
     else:
         file_manager.set_strategy(Another())
@@ -29,6 +59,16 @@ def ensure_file_format(file: str) -> Optional[str]:
     return path
 
 def make_ocr(file: str) -> List[Dict]:
+    """
+    Function to extract text from a file using OCR.
+
+    Args:
+        file (str): The path of the file.
+    
+    Returns:
+        List[Dict]: A list of dictionaries containing the text and metadata of the file.
+    """
+
     if file is not None and file.lower().endswith('.pdf'):
         data = OCR.get_ocr(file)
     else:
@@ -36,25 +76,55 @@ def make_ocr(file: str) -> List[Dict]:
     return data
 
 def get_text_chunks(ocr_text: List[Dict]) -> pl.DataFrame:
-    text_chunk = TextChunk()
-    df_text = text_chunk.add_chunks_to_dataframe(ocr_text)
-    text_chunk.save_checkpoint('./data/checkpoint.db')
+    """
+    Function to add text chunks to a Polars DataFrame.
+
+    Args:
+        ocr_text (List[Dict]): A list of dictionaries containing the text and metadata of the file.
+    
+    Returns:
+        pl.DataFrame: A Polars DataFrame with the text chunks
+    """
+
+    data_df = pl.DataFrame({})
+    df_text = TextChunk.add_chunks_to_dataframe(data_df, ocr_text)
+    TextChunk.save_checkpoint(df_text, './data/checkpoint.db')
     return df_text
 
 def insert_points(df_points: pl.DataFrame) -> None:
-    milvus_manager = MilvusManager("collection")
-    milvus_manager.create_collection()
-    milvus_manager.insert_points(df_points)
+    """
+    Function to insert points in Milvus.
 
-def get_context(query: str):
-    milvus_manager = MilvusManager("collection")
-    data = milvus_manager.search_points(query)
+    Args:
+        df_points (pl.DataFrame): The DataFrame containing the text column.
+
+    Returns:
+        None
+    """
+
+    milvus_manager.create_collection("collection")
+    milvus_manager.insert_points("collection", df_points)
+
+def get_context(query: str) -> List[str]:
+    """
+    Function to get the context of a query.
+
+    Args:
+        query (str): The query to search for.
+
+    Returns:
+        List[str]: A list of strings with the context of the query.
+    """
+
+    data = milvus_manager.search_points("collection", query)
     points = json.loads(data)
     context = []
     for point in points[0]:
         context.append(point['entity'])
     return context
 
+
+# Routes: /hello, /upload, /search
 
 @app.get("/hello")
 def hello():
@@ -68,9 +138,9 @@ def upload_file():
     
     file = request.files['file']
     if file.filename == '':
-        return jsonify({"error": "No file has been sent: 'file'"}), 400
+        return jsonify({"error": "No file has been sent: 'file'"}), 401
     
-    if file and allowed_file(file.filename):
+    if file and allowed_file(file.filename, ALLOWED_EXTENSIONS):
         filename = secure_filename(file.filename)
         path_file = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         file.save(path_file)
--- core/__init__.py
+++ core/__init__.py
@@ -2,4 +2,4 @@ from .files_strategy import FileManager, AcceptedFiles, Another
 from .OCR import OCR
 from .TextChunk import TextChunk
 from .Milvus import MilvusManager
-from .utils import get_logger
+from .utils import get_logger, delete_files_directory
